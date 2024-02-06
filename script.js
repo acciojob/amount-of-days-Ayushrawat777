edffb0083a52714c0b1200e6399effb6b101cc0b
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-02-07 00:33:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// program to check leap year
+function daysOfAYear(year) {
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return 366
+    } else {
+       return 365
+    }
+}
+
+// take input
+ const year = prompt('Enter a year:'); 
+ daysOfAYear(year); 
+alert(daysOfAYear(year));
